c6c745833ee3666c9d4e696003e41c6df3416501
Mustafa Genc <mustafagenc20@gmail.com>
2022-04-04 16:58:48 +0200

    initial commits

--- src/main/java/com/kindsonthegenius/fleetapp/controllers/LocationController.java
+++ src/main/java/com/kindsonthegenius/fleetapp/controllers/LocationController.java
@@ -1,4 +1,4 @@
-package com.kindsonthegenius.fleetapp.controller;
+package com.kindsonthegenius.fleetapp.controllers;
 
 import java.util.List;
 import java.util.Optional;
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.kindsonthegenius.fleetms.models.Location;
-import com.kindsonthegenius.fleetms.services.CountryService;
-import com.kindsonthegenius.fleetms.services.LocationService;
-import com.kindsonthegenius.fleetms.services.StateService;
+import com.kindsonthegenius.fleetapp.models.Location;
+import com.kindsonthegenius.fleetapp.services.CountryService;
+import com.kindsonthegenius.fleetapp.services.LocationService;
+import com.kindsonthegenius.fleetapp.services.StateService;
 
 @Controller
 @RequestMapping("/locations")
