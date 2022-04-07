46a163fdce89428a016ec94d824bb3cc907ac4dc
Mustafa Genc <mustafagenc20@gmail.com>
2022-04-07 14:57:51 +0200

    initial commits

--- src/main/java/com/kindsonthegenius/fleetapp/controllers/VehicleMovementController.java
+++ src/main/java/com/kindsonthegenius/fleetapp/controllers/VehicleMovementController.java
@@ -1,4 +1,4 @@
-package com.kindsonthegenius.fleetapp.controller;
+package com.kindsonthegenius.fleetapp.controllers;
 
 import java.util.Optional;
 
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.kindsonthegenius.fleetms.models.VehicleMovement;
-import com.kindsonthegenius.fleetms.services.LocationService;
-import com.kindsonthegenius.fleetms.services.VehicleMovementService;
-import com.kindsonthegenius.fleetms.services.VehicleService;
+import com.kindsonthegenius.fleetapp.models.VehicleMovement;
+import com.kindsonthegenius.fleetapp.services.LocationService;
+import com.kindsonthegenius.fleetapp.services.VehicleMovementService;
+import com.kindsonthegenius.fleetapp.services.VehicleService;
 
 @Controller
 public class VehicleMovementController {
