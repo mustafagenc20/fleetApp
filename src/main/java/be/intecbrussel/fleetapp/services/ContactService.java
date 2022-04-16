402dc12cda1537ad4bf2e6a8b10006789c45abe3
Mustafa Genc <mustafagenc20@gmail.com>
2022-04-16 19:41:49 +0200

    initial commit

--- src/main/java/be/intecbrussel/fleetapp/services/ContactService.java
+++ src/main/java/be/intecbrussel/fleetapp/services/ContactService.java
@@ -1,13 +1,13 @@
-package com.kindsonthegenius.fleetapp.services;
+package be.intecbrussel.fleetapp.services;
 
 import java.util.List;
 import java.util.Optional;
 
+import be.intecbrussel.fleetapp.repositories.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.kindsonthegenius.fleetapp.models.Contact;
-import com.kindsonthegenius.fleetapp.repositories.ContactRepository;
+import be.intecbrussel.fleetapp.models.Contact;
 
 @Service
 public class ContactService {
