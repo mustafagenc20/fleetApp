c3c44d9a983dc656901e9794e14c01ac3de3c1b8
Mustafa Genc <mustafagenc20@gmail.com>
2022-04-16 19:41:21 +0200

    initial commit

--- src/main/java/be/intecbrussel/fleetapp/repositories/CountryRepository.java
+++ src/main/java/be/intecbrussel/fleetapp/repositories/CountryRepository.java
@@ -1,9 +1,9 @@
-package com.kindsonthegenius.fleetapp.repositories;
+package be.intecbrussel.fleetapp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.kindsonthegenius.fleetapp.models.Country;
+import be.intecbrussel.fleetapp.models.Country;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Integer> {
