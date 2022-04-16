11d5c5d0a6ea00717699e2ca3128afdc08232d0f
Mustafa Genc <mustafagenc20@gmail.com>
2022-04-16 19:41:30 +0200

    initial commit

--- src/main/java/be/intecbrussel/fleetapp/services/ClientService.java
+++ src/main/java/be/intecbrussel/fleetapp/services/ClientService.java
@@ -1,13 +1,13 @@
-package com.kindsonthegenius.fleetapp.services;
+package be.intecbrussel.fleetapp.services;
 
 import java.util.List;
 import java.util.Optional;
 
+import be.intecbrussel.fleetapp.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.kindsonthegenius.fleetapp.models.Client;
-import com.kindsonthegenius.fleetapp.repositories.ClientRepository;
+import be.intecbrussel.fleetapp.models.Client;
 
 @Service
 public class ClientService {
