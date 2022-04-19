2fcdc8d4e943a3c7997a76cc4a0d49be71ca2979
Mustafa Genc <mustafagenc20@gmail.com>
2022-04-19 23:40:44 +0200

    Initial commit

--- src/main/java/be/intecbrussel/fleetapp/services/SupplierService.java
+++ src/main/java/be/intecbrussel/fleetapp/services/SupplierService.java
@@ -1,13 +1,13 @@
-package com.kindsonthegenius.fleetapp.services;
+package be.intecbrussel.fleetapp.services;
 
 import java.util.List;
 import java.util.Optional;
 
+import be.intecbrussel.fleetapp.repositories.SupplierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.kindsonthegenius.fleetapp.models.Supplier;
-import com.kindsonthegenius.fleetapp.repositories.SupplierRepository;
+import be.intecbrussel.fleetapp.models.Supplier;
 
 @Service
 public class SupplierService {
